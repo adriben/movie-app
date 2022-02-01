cabb5be4047fa45024b4021cc5675a45085ca20f
Adrien Benete <adrbenete@gmail.com>
2022-02-01 16:15:22 +0900

    ajout des la page pour single movie

--- pages/movies/_movieid.vue
+++ pages/movies/_movieid.vue
@@ -1,7 +1,34 @@
 <template>
 <Loading-spiner v-if="$fetchState.pending"/>
     <div v-else class="container single-movie">
-        <h1>{{ this.movie.title }}</h1>
+        <NuxtLink class="button" :to="{ name: 'index' }">Back</NuxtLink>
+        <div class="movie-info">
+            <div class="movie-img">
+                <img
+              :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+              alt=""
+            />
+            </div>
+            <div class="movie-content">
+                <h1>Title: {{ movie.title }}</h1>
+                <p class="movie-fact tagline">
+                    <span>Tagline:</span> "{{ movie.tagline }}"
+                </p>
+                <p class="movie-fact">
+                 <span>Released:</span>
+                  {{ movie.release_date }}
+
+                </p>
+                <p class="movie-fact">
+                    <span>Duration:</span>
+                     {{ movie.runtime }} minutes
+                </p>
+                <p class="movie-fact">
+                    <span>Overview:</span>
+                      {{ movie.overview }}
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 <script >
@@ -25,7 +52,7 @@ export default {
     methods: {
         async getSingleMovie() {
         
-        const data = axios.get(`https://api.themoviedb.org/3/movie/${this.$route.movieid}?api_key=65edaf62da630f3d1bc494487dfe9722&language=en-US`)
+        const data = axios.get(`https://api.themoviedb.org/3/movie/${this.$route.params.movieid}?api_key=65edaf62da630f3d1bc494487dfe9722&language=en-US`)
         
         const result = await data
         this.movie = result.data
@@ -37,6 +64,59 @@ export default {
 
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.single-movie {
+  color: #fff;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 32px 16px;
+  .button {
+    align-self: flex-start;
+    margin-bottom: 32px;
+  }
+  .movie-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    color: #fff;
+    @media (min-width: 800px) {
+      flex-direction: row;
+      align-items: flex-start;
+    }
+    .movie-img {
+      img {
+        max-height: 500px;
+        width: 100%;
+        @media (min-width: 800px) {
+          max-height: 700px;
+          width: initial;
+        }
+      }
+    }
+    .movie-content {
+      h1 {
+        font-size: 56px;
+        font-weight: 400;
+      }
+      .movie-fact {
+        margin-top: 12px;
+        font-size: 20px;
+        line-height: 1.5;
+        span {
+          font-weight: 600;
+          text-decoration: underline;
+        }
+      }
+      .tagline {
+        font-style: italic;
+        span {
+          font-style: normal;
+        }
+      }
+    }
+  }
+}
 </style>
